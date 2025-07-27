8f40948d43df0b360297b0a3f4caa175a88cb60f
Gildácio Lopes <gildacioj399@gmail.com>
2025-07-27 06:31:56 -0300

    chore: frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+// 1) Converte import.meta.url para nome de arquivo
+const __filename = fileURLToPath(import.meta.url);
+// 2) Deriva o diretório
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
